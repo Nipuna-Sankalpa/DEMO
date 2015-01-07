2f610f772d1ea3641b2c8a8769c5866158e8b3cf
Pubudu Fernando <pubudu.fernando@gmail.com>
2015-01-08 00:24:05 +0530

    test commit

--- src/demo/DEMO.java
+++ src/demo/DEMO.java
@@ -16,7 +16,7 @@ public class DEMO {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("HELLO CONFLICTS!");
+        System.out.println("HELLOOOO CONFLICTS!");
         
         for (int i = 0; i < 10; i++) {
             System.out.println("psd solutions");
